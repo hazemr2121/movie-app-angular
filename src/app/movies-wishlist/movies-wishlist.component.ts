13f7d3fed1e89abfe301abecd9b91e99e291bef0
sohailaabdelazeem <sohailaabdelazeem863@gmail.com>
2025-02-08 16:37:25 +0200

    use wishlist service

--- src/app/movies-wishlist/movies-wishlist.component.ts
+++ src/app/movies-wishlist/movies-wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { WishlistService } from '../services/wishlist.service';
+import { Movie } from '../movie.inteface';
 
 @Component({
   selector: 'app-movies-wishlist',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './movies-wishlist.component.css'
 })
 export class MoviesWishlistComponent {
+  wishlistMovies: Movie[] = [];
 
+
+  constructor(private router: Router,private wishlistService: WishlistService) {}
+  ngOnInit() {
+    this.wishlistService.getWishlist().subscribe(movies => {
+      this.wishlistMovies = movies;
+    });
+  }
+
+  removeFromWishlist(movieId: number) {
+    this.wishlistService.removeFromWishlist(movieId);
+  }
+
+  goToMovies(){
+    this.router.navigate(['/']); 
+  }
 }
