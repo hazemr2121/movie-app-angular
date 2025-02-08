541f9ef76b788a96c73e7707e865123cb88b75c2
MoodyEG <moodyeg2011@yahoo.com>
2025-02-08 22:24:31 +0200

    Get the movies from API

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+import { Observable } from 'rxjs';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [],
+  imports: [MovieCardComponent],
   templateUrl: './movies-list.component.html',
-  styleUrl: './movies-list.component.css'
+  styleUrl: './movies-list.component.css',
 })
 export class MoviesListComponent {
-
+  nowPlaying: any;
+  constructor(private movieService: MovieService) {
+    this.movieService.getNowPlaying().subscribe((data) => {
+      this.nowPlaying = data.results;
+    });
+  }
 }
