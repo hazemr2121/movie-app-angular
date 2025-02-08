e7fb78af5137834304292a649eeced9af765730c
MoodyEG <moodyeg2011@yahoo.com>
2025-02-08 22:24:12 +0200

    Modifications to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { WishlistService } from './services/wishlist.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(public wishlistService: WishlistService) {}
   title = 'movie-app-angular';
 }
