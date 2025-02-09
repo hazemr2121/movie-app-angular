ba0a9b2a37f3712a151f0735bad080efbdb2514e
MoodyEG <moodyeg2011@yahoo.com>
2025-02-09 17:12:15 +0200

    Added paginations and search results

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,19 +1,69 @@
 import { Component } from '@angular/core';
 import { MovieService } from '../services/movie.service';
-import { Observable } from 'rxjs';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [MovieCardComponent],
+  imports: [MovieCardComponent, FormsModule],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.css',
 })
 export class MoviesListComponent {
   nowPlaying: any;
+  page: number = 1;
+  query: any;
+  search: boolean = false;
   constructor(private movieService: MovieService) {
-    this.movieService.getNowPlaying().subscribe((data) => {
+    this.movieService.getNowPlaying(this.page).subscribe((data) => {
       this.nowPlaying = data.results;
     });
+  }
+
+  nextPage() {
+    if (this.nowPlaying.length < 20) return;
+    if (this.search) {
+      this.movieService
+        .searchMovies(this.query, ++this.page)
+        .subscribe((data) => {
+          this.nowPlaying = data.results;
+        });
+      return;
+    }
+    this.movieService.getNowPlaying(++this.page).subscribe((data) => {
+      this.nowPlaying = data.results;
+    });
+  }
+
+  prevPage() {
+    if (this.page <= 1) return;
+    if (this.search) {
+      this.movieService
+        .searchMovies(this.query, --this.page)
+        .subscribe((data) => {
+          this.nowPlaying = data.results;
+        });
+      return;
+    }
+    this.movieService.getNowPlaying(--this.page).subscribe((data) => {
+      this.nowPlaying = data.results;
+    });
+  }
+
+  searchMovies(query: string) {
+    if (!query) {
+      this.search = false;
+      this.movieService.getNowPlaying(this.page).subscribe((data) => {
+        this.nowPlaying = data.results;
+      });
+    } else {
+      this.search = true;
+      this.page = 1;
+      this.movieService
+        .searchMovies(this.query, this.page)
+        .subscribe((data) => {
+          this.nowPlaying = data.results;
+        });
+    }
   }
 }
