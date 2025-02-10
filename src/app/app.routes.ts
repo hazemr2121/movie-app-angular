178d1e6414d719f195b336041e26c3eb2c6aa78b
Mohamed Abo Saeed <133701978+mohamed-saeed19@users.noreply.github.com>
2025-02-10 23:48:31 +0200

    add 404 page & edit routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { MoviesListComponent } from './movies-list/movies-list.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesWishlistComponent } from './movies-wishlist/movies-wishlist.component';
 import { MoviesSearchResultsComponent } from './movies-search-results/movies-search-results.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: MoviesListComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
   { path: 'wishlist', component: MoviesWishlistComponent },
   { path: 'search', component: MoviesSearchResultsComponent },
-  { path: '**', redirectTo: '' },
+  { path: '**', component:NotFoundComponent },
 ];
