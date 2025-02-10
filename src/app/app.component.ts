64a4096de554e40fbe6cfb29bcfa3a0b7ff254d6
MoodyEG <moodyeg2011@yahoo.com>
2025-02-10 14:18:24 +0200

    Added static drop down to language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { WishlistService } from './services/wishlist.service';
 import { AsyncPipe } from '@angular/common';
+import { LanguageService } from './services/language.service';
+import { MovieService } from './services/movie.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,17 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  constructor(public wishlistService: WishlistService) {}
+  constructor(
+    public wishlistService: WishlistService,
+    public languageService: LanguageService,
+    public movieService: MovieService
+  ) {}
   title = 'movie-app-angular';
+
+  changeLang(lang: string) {
+    this.languageService.setLanguage(lang);
+    this.movieService
+      .getCurrentLanguage()
+      .subscribe((data) => console.log(data));
+  }
 }
